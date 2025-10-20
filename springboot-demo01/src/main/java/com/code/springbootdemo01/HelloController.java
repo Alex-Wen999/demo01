55645bd793f223f5c4c4df139f1a8d11d27fa359
Alex-Wen999 <1410973156@qq.com>
2025-10-20 10:46:59 +0800

    初次提交

---
+++ springboot-demo01/src/main/java/com/code/springbootdemo01/HelloController.java
@@ -0,0 +1,19 @@
+package com.code.springbootdemo01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: hello
+ * @Author:
+ * @date: 2025.10.20
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
